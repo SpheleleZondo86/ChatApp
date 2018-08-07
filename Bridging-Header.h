815d3f425a0c04cf56353777e61494f6fa1d7c76
Sphelele Zondo <sphelelez@ftcadms-macbook-pro.local>
2018-08-07 16:23:13 +0200

    add cocoa pods

---
+++ Bridging-Header.h
@@ -0,0 +1,9 @@
+//
+//  Bridging-Header.h
+//  ChatApp
+//
+//  Created by Sphelele Zondo on 2018/08/07.
+//  Copyright © 2018 SpheleleZondo. All rights reserved.
+//
+
+#import <JSQMessagesViewController/JSQMessages.h>
---
+++ ChatApp/ChatViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ChatApp
+//
+//  Created by Sphelele Zondo on 2018/08/07.
+//  Copyright © 2018 SpheleleZondo. All rights reserved.
+//
+
+import UIKit
+
+class ChatViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
